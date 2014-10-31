109067d1db11becedbb2ab446ccf53e8f5a84d1f
Gleb <gleb.satyukov@gmail.com>
2014-10-31 11:49:13 +0100

    Moved twitter stream into an init function, waiting for user to enter search term.

--- server.js
+++ server.js
@@ -21,10 +21,21 @@ var serialport = new SerialPort(
     stopBits: 1
     }, false);
 
-var HASHTAG = 'Happy Halloween';
+var HASHTAG = '';
 var tweetCount = 0;
 var tweetTotalPolarity = 0;
 var tweetPolarity = 0;
+
+console.log('Enter search term: \n');
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (input) {
+    if ( !input ) return;
+    input = input.replace(/(\r\n|\n|\r)/gm,"");
+    console.log('Initializing sentiment analysis for \"' + input + '\", hold on.....\n\n\n');
+    HASHTAG = input;
+    init();
+});
 
 var sendData = function(data){
     serialport.open( function(err) {
@@ -35,21 +46,23 @@ var sendData = function(data){
     });
 };
 
-twit.verifyCredentials(function (err, data) {
-    if ( err ) return console.error("Error connecting to Twitter: " + err);
-    stream = twit.stream('statuses/filter', {
-        'track': HASHTAG
-    }, function (stream) {
-        console.log("Monitoring Twitter for \'" + HASHTAG + "\'...  Logging Twitter traffic.");
-        stream.on('data', function (data) {
-            if (data.lang === 'en') {
-                sentiment(data.text, function (err, result) {
-                    tweetCount++;
-                    tweetTotalPolarity += result.score;
-                    tweetPolarity = tweetTotalPolarity / tweetCount;
-                    console.log('Tweet count: ' + tweetCount + '; polarity: ' + tweetPolarity + '; tweet: ' + data.text);
-                });
-            }
+var init = function () {
+    twit.verifyCredentials(function (err, data) {
+        if ( err ) return console.error("Error connecting to Twitter: " + err);
+        stream = twit.stream('statuses/filter', {
+            'track': HASHTAG
+        }, function (stream) {
+            console.log('Monitoring Twitter for \'' + HASHTAG + '\'...  Logging Twitter traffic.');
+            stream.on('data', function (data) {
+                if (data.lang === 'en') {
+                    sentiment(data.text, function (err, result) {
+                        tweetCount++;
+                        tweetTotalPolarity += result.score;
+                        tweetPolarity = tweetTotalPolarity / tweetCount;
+                        console.log('Tweet count: ' + tweetCount + '; polarity: ' + tweetPolarity + '; tweet: ' + data.text);
+                    });
+                }
+            });
         });
     });
-});
+};
